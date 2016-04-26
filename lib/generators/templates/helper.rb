345bbe6d7c0749eea00f87a51d15a614afc0caf3
Tom Dallimore <me@tomdallimore.com>
2016-04-26 21:08:13 +0700

    Fixing syntax issues.

--- lib/generators/templates/helper.rb
+++ lib/generators/templates/helper.rb
@@ -1,6 +1,6 @@
 module PaypalHelper
 
     def paypal_form_tag
-        raw("<div class='paypal-form-wrapper'>#{image_tag('paypal-icon.png')}#{radio_button_tag(:payment_type, 'paypal' checked: checked)}</div>")
+        raw("<div class='paypal-form-wrapper'>#{image_tag('paypal-icon.png')}#{radio_button_tag(:payment_type, 'paypal', checked: true)}</div>")
     end
 end
